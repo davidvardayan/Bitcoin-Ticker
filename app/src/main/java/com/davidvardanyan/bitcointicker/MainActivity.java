350ce14b75c172d39a7ef1acae6e5b9077532c0b
David <dav-ss@mail.ru>
2019-05-28 06:54:56 -0700

    Importing assets,Creating spinner,attaching text and button views to java code

---
+++ app/src/main/java/com/davidvardanyan/bitcointicker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.davidvardanyan.bitcointicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private final String BASE_URL = "";
+
+    // Member Variables:
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
+        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+
+        spinner.setAdapter(adapter);
+
+
+    }
+
+
+
+
+
+}
